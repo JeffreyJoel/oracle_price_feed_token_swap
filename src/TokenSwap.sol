c2ee9818a748a3b9b63ed80662e47dfaa1ad87fb
JeffreyJoel <jowoloko@gmail.com>
2024-03-26 02:49:14 +0100

    update swap with price feed

--- src/TokenSwap.sol
+++ src/TokenSwap.sol
@@ -1,58 +1,109 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "./interfaces/IERC20.sol";
+import {AggregatorV3Interface} from "lib/chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {FeedRegistryInterface} from "lib/chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
 
 contract TokenSwap {
     address public ethToken;
     address public linkToken;
     address public daiToken;
 
-    constructor(address _ethToken, address _linkToken, address _daiToken) {
+    address private feedRegistryAddress;
+
+    FeedRegistryInterface private feedRegistry;
+
+    event EthToLinkSwap(address indexed sender, uint256 amount);
+    event EthToDaiSwap(address indexed sender, uint256 amount);
+    event LinkToEthSwap(address indexed sender, uint256 amount);
+    event LinkToDaiSwap(address indexed sender, uint256 amount);
+    event DaiToEthSwap(address indexed sender, uint256 amount);
+    event DaiToLinkSwap(address indexed sender, uint256 amount);
+
+    constructor(
+        address _ethToken,
+        address _linkToken,
+        address _daiToken,
+        address _feedRegistryAddress
+    ) {
         ethToken = _ethToken;
         linkToken = _linkToken;
         daiToken = _daiToken;
+        feedRegistryAddress = _feedRegistryAddress;
     }
 
     function swapEthToLink(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(ethToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(linkToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(ethToken, linkToken, amount);
+        IERC20(linkToken).transfer(msg.sender, amountOut);
+
+        emit EthToLinkSwap(msg.sender, amount);
     }
 
     function swapEthToDai(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(ethToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(daiToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(ethToken, daiToken, amount);
+        IERC20(daiToken).transfer(msg.sender, amountOut);
+
+        emit EthToDaiSwap(msg.sender, amount);
     }
 
     function swapLinkToEth(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(linkToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(ethToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(linkToken, ethToken, amount);
+        IERC20(ethToken).transfer(msg.sender, amountOut);
+
+        emit LinkToEthSwap(msg.sender, amount);
     }
 
     function swapLinkToDai(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(linkToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(daiToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(linkToken, daiToken, amount);
+        IERC20(daiToken).transfer(msg.sender, amountOut);
+
+        emit LinkToDaiSwap(msg.sender, amount);
     }
 
     function swapDaiToEth(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(daiToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(ethToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(daiToken, ethToken, amount);
+        IERC20(ethToken).transfer(msg.sender, amountOut);
+
+        emit DaiToEthSwap(msg.sender, amount);
     }
 
     function swapDaiToLink(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(daiToken).transferFrom(msg.sender, address(this), amount);
-        IERC20(linkToken).transfer(msg.sender, amount);
+        uint256 amountOut = _handleAmountOut(daiToken, linkToken, amount);
+        IERC20(linkToken).transfer(msg.sender, amountOut);
+
+        emit DaiToLinkSwap(msg.sender, amount);
+    }
+
+    function _handleAmountOut(
+        address base,
+        address quote,
+        uint256 amountIn
+    ) internal view returns (uint256) {
+        (, int256 priceIn, , , ) = feedRegistry.latestRoundData(base, quote);
+        require(priceIn > 0, "Price feed not available for base asset");
+        return uint256(amountIn * uint256(priceIn));
     }
 }
+
+//0x779877A7B0D9E8603169DdbD7836e478b4624789 Link sepolia
+//0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9 eth sepolia
+// 0x68194a729C2450ad26072b3D33ADaCbcef39D574 dai sepolia
