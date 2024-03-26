9458fdc7ad24c1ca43e94a49bde2b112352c1fb3
JeffreyJoel <jowoloko@gmail.com>
2024-03-26 10:21:24 +0100

    update contract tests

--- test/TokenSwap.t.sol
+++ test/TokenSwap.t.sol
@@ -17,41 +17,53 @@ contract tokenSwapTest is Test {
     IERC20 dai;
     IERC20 link;
     IERC20 weth;
+    IERC20 usd;
 
     TokenSwap tokenSwap;
+    address ETH_USD_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    address DAI_USD_FEED = 0x14866185B1962B63C3Ea9E03Bc1da838bab34C19;
 
-    address daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address linkAddress = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
-    address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address feedRegistryAddress = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
+    address USDAddress = 0xf08A50178dfcDe18524640EA6618a1f965821715;
+    address DAIAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address WETHAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    address LINKAddress = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
 
     address prankAddress = 0x40ec5B33f54e0E8A33A975908C5BA1c14e5BbbDf;
 
     function setUp() public {
         tokenSwap = new TokenSwap(
-            wethAddress,
-            linkAddress,
-            daiAddress,
-            feedRegistryAddress
+            WETHAddress,
+            LINKAddress,
+            USDAddress,
+            DAIAddress,
+            DAI_USD_FEED,
+            ETH_USD_FEED
         );
 
-        dai = IERC20(daiAddress);
-        link = IERC20(linkAddress);
-        weth = IERC20(wethAddress);
+        dai = IERC20(DAIAddress);
+        link = IERC20(LINKAddress);
+        weth = IERC20(WETHAddress);
+        usd = IERC20(USDAddress);
 
         vm.startPrank(prankAddress);
     }
 
-    function testSwapEthToLink() public {
-        uint256 linkAmount = link.balanceOf(msg.sender);
-        console.log("linkAmount: ", linkAmount);
-        require(linkAmount > 0, "Insufficient LINK balance");
-        link.approve(address(tokenSwap), linkAmount);
-        tokenSwap.swapEthToLink(linkAmount);
+    function testSwapDAIToUSD() public {
+        uint256 daiBalance = dai.balanceOf(msg.sender);
+        console.log("DAI balance: ", daiBalance);
+
+        require(daiBalance > 0, "You have Insufficient DAI");
+        dai.approve(address(tokenSwap), daiBalance);
+        tokenSwap.swapDAIToUSD(daiBalance);
     }
 
-    // function testFuzz_SetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
+    function testSwapETHForUSD() public {
+        uint256 wethBalance = weth.balanceOf(msg.sender);
+        console.log("initial ETH balance: ", wethBalance);
+
+        require(wethBalance > 0, "You have Insufficient Weth");
+        weth.approve(address(tokenSwap), wethBalance);
+        tokenSwap.swapEthToUSD(wethBalance);
+    }
 }
