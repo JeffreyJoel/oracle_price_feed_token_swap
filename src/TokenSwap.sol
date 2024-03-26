72f3ce1a3b787ac6db560ccab0ecec614ff14127
JeffreyJoel <jowoloko@gmail.com>
2024-03-26 09:26:14 +0100

    update contract

--- src/TokenSwap.sol
+++ src/TokenSwap.sol
@@ -9,101 +9,68 @@ contract TokenSwap {
     address public ethToken;
     address public linkToken;
     address public daiToken;
+    address public usd;
 
     address private feedRegistryAddress;
 
     FeedRegistryInterface private feedRegistry;
 
-    event EthToLinkSwap(address indexed sender, uint256 amount);
-    event EthToDaiSwap(address indexed sender, uint256 amount);
-    event LinkToEthSwap(address indexed sender, uint256 amount);
-    event LinkToDaiSwap(address indexed sender, uint256 amount);
-    event DaiToEthSwap(address indexed sender, uint256 amount);
-    event DaiToLinkSwap(address indexed sender, uint256 amount);
+    event EthToUsdSwap(address indexed sender, uint256 ethAmount, uint256 usdAmount);
+    event DaiToUsdSwap(address indexed sender, uint256 ethAmount, uint256 daiAmount);
+    event LinkToEthSwap(address indexed sender, uint256 linkAmount, uint256 ethAmount);
+    event LinkToDaiSwap(address indexed sender, uint256 linkAmount, uint256 daiAmount);
+    event DaiToEthSwap(address indexed sender, uint256 daiAmount, uint256 ethAmount);
+    event DaiToLinkSwap(address indexed sender, uint256 daiAmount, uint256 linkAmount);
+
+    address public immutable eth_usd_address;
+    address public immutable dai_usd_address;
+
+    AggregatorV3Interface public immutable DAI_USD_FEED;
+    AggregatorV3Interface public immutable ETH_USD_FEED;
 
     constructor(
         address _ethToken,
         address _linkToken,
         address _daiToken,
-        address _feedRegistryAddress
+        address _usd,
+        address _daiUsdFeed,
+        address _ethUsdFeed
     ) {
         ethToken = _ethToken;
         linkToken = _linkToken;
+        usd = _usd;
         daiToken = _daiToken;
-        feedRegistryAddress = _feedRegistryAddress;
+        DAI_USD_FEED = AggregatorV3Interface(_daiUsdFeed);
+        ETH_USD_FEED = AggregatorV3Interface(_ethUsdFeed);
     }
 
-    function swapEthToLink(uint256 amount) external {
+    function swapEthToUSD(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(ethToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(ethToken, linkToken, amount);
-        IERC20(linkToken).transfer(msg.sender, amountOut);
+        uint256 amountOut = _handleAmountOut(ETH_USD_FEED, amount);
+        IERC20(usd).transfer(msg.sender, amountOut);
 
-        emit EthToLinkSwap(msg.sender, amount);
+        emit EthToUsdSwap(msg.sender, amount, amountOut);
     }
 
-    function swapEthToDai(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        IERC20(ethToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(ethToken, daiToken, amount);
-        IERC20(daiToken).transfer(msg.sender, amountOut);
-
-        emit EthToDaiSwap(msg.sender, amount);
-    }
-
-    function swapLinkToEth(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        IERC20(linkToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(linkToken, ethToken, amount);
-        IERC20(ethToken).transfer(msg.sender, amountOut);
-
-        emit LinkToEthSwap(msg.sender, amount);
-    }
-
-    function swapLinkToDai(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        IERC20(linkToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(linkToken, daiToken, amount);
-        IERC20(daiToken).transfer(msg.sender, amountOut);
-
-        emit LinkToDaiSwap(msg.sender, amount);
-    }
-
-    function swapDaiToEth(uint256 amount) external {
+    function swapDAIToUSD(uint256 amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
         IERC20(daiToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(daiToken, ethToken, amount);
-        IERC20(ethToken).transfer(msg.sender, amountOut);
+        uint256 amountOut = _handleAmountOut(DAI_USD_FEED, amount);
+        IERC20(usd).transfer(msg.sender, amountOut);
 
-        emit DaiToEthSwap(msg.sender, amount);
+        emit DaiToUsdSwap(msg.sender, amount, amountOut);
     }
 
-    function swapDaiToLink(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        IERC20(daiToken).transferFrom(msg.sender, address(this), amount);
-        uint256 amountOut = _handleAmountOut(daiToken, linkToken, amount);
-        IERC20(linkToken).transfer(msg.sender, amountOut);
-
-        emit DaiToLinkSwap(msg.sender, amount);
-    }
 
     function _handleAmountOut(
-        address base,
-        address quote,
+         AggregatorV3Interface feed,
         uint256 amountIn
     ) internal view returns (uint256) {
-        (, int256 priceIn, , , ) = feedRegistry.latestRoundData(base, quote);
+        (, int256 priceIn, , , ) = feed.latestRoundData();
         require(priceIn > 0, "Price feed not available for base asset");
         return uint256(amountIn * uint256(priceIn));
     }
 }
-
-//0x514910771AF9Ca656af840dff83E8264EcF986CA Link mainnet
-//0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 eth mainnet
-// 0x6B175474E89094C44Da98b954EedeAC495271d0F dai mainnet
